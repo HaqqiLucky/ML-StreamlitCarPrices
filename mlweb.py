054b8fe3156f6fe3d08e5fd5c7d019a2e9736510
HaqqiLucky <haqqiarifin@outlook.com>
2024-11-30 07:43:30 +0700

    hapus file yg tdk guna

--- mlweb.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-angka = st.number_input(label="Pick a number",min_value=0)
-email = st.text_input(label="Email Address")
-tanggal = st.date_input(label="Travelling Date")
-jam = st.time_input(label="School Time")
-desk = st.text_area(label="Deskripsi", height=68)
-file = st.file_uploader("Upload foto", type=["jpg","jpeg"])
-color = st.color_picker(label="Choose your fav color")
